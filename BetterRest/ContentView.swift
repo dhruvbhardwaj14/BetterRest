ccd5eafc4d3ad70cbc156d3e09ad08d87faab462
dhruvbhardwaj14 <dhruv9891891827@gmail.com>
2022-06-22 22:58:58 +0530

    Initial Commit

---
+++ BetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Dhruv Bhardwaj on 22/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
